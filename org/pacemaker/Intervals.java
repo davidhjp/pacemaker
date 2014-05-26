52447fa3b51925851c823087dd2db4a143887269
Heejong Park <hpar081@aucklanduni.ac.nz>
2014-05-26 16:54:53 +1200

    writing atriumsensor

---
+++ org/pacemaker/Intervals.java
@@ -0,0 +1,11 @@
+package org.pacemaker;
+
+public class Intervals {
+	public static final int P = 110;
+	public static final int QRS = 100;
+	public static final int AVI = 150;
+	public static final int VAI = 850;
+	public static final int PVARP = 350;
+	public static final int MSI = 500;
+	
+}
